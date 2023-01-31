d6d2a2178b469f5812125c839c584dc5bb6fee03
Ben Cubin <bencubin@gmail.com>
2023-01-31 21:26:27 +0600

    split into components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p>
+                Inspired by <a href="https://qlocktwo.com/ww/">QLOCKTWO</a>. 
+                Made by <a href="https://github.com/bencubin">Bencubin</a>, 
+                with awesome font by <a href="https://shriftovik.com/">Shriftovik</a>
+            </p>
+        </footer>
+    );
+}
